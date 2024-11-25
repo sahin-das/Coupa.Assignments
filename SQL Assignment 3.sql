787b77024e9fd118ba49eb7fcff6b51a1478721b
sahin-das <sahin.das@coupa.com>
2024-11-25 08:43:39 +0530

    Update SQL Assignment 3.sql

--- SQL Assignment 3.sql
+++ SQL Assignment 3.sql
@@ -3,15 +3,14 @@
 
 use AdventureWorks2022;
 
-with AccountNumbers as (
-	SELECT AccountNumber, SUM(LineTotal) as TotalSpend
-	FROM Sales.SalesOrderDetail so
-	JOIN Sales.SalesOrderHeader sh
-	ON so.SalesOrderID = sh.SalesOrderID
-	GROUP BY AccountNumber
-	HAVING SUM(LineTotal) > 70000
+select top (5) SOH.AccountNumber, SOH.SalesOrderID, SOH.OrderDate
+FROM Sales.SalesOrderHeader SOH
+JOIN Sales.SalesOrderDetail SOD
+    ON SOH.SalesOrderID = SOD.SalesOrderID
+WHERE SOH.AccountNumber IN (
+    SELECT AccountNumber
+    FROM Sales.SalesOrderHeader
+    GROUP BY AccountNumber
+    HAVING SUM(TotalDue) > 70000
 )
-
-select top (5) so.AccountNumber, TotalSpend, SalesOrderID, OrderDate from Sales.SalesOrderHeader as so
-right JOIN AccountNumbers an ON so.AccountNumber = an.AccountNumber
-order by OrderDate desc;
+ORDER BY SOH.OrderDate DESC;
